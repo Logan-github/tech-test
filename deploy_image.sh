c4bc77ae66e20ea8cc5e2babf144cd6edb15977f
Loganathan sakthivel <loganathansakthivel@loganathans-air.home>
2021-12-21 16:39:21 +0000

    fixed issue

--- deploy_image.sh
+++ deploy_image.sh
@@ -2,5 +2,5 @@ echo "Starting to deploy docker image.."
 DOCKER_IMAGE=demo-app
 CONTAINER_NAME=demo-app-nginx
 sudo docker pull $DOCKER_IMAGE
-sudo docker ps -q --filter ancestor=$DOCKER_IMAGE | xargs docker stop
+sudo docker ps -q --filter ancestor=$DOCKER_IMAGE | xargs sudo docker stop
 sudo docker container run --name $CONTAINER_NAME -p 80:80 -dit $DOCKER_IMAGE
