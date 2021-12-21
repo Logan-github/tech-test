7bb5e2868b7751e74c84bdf48d788df05917236d
Loganathan sakthivel <loganathansakthivel@loganathans-air.home>
2021-12-21 23:06:18 +0000

    updated ssl conf

--- deploy_image.sh
+++ deploy_image.sh
@@ -1,6 +1,7 @@
 echo "Starting to deploy docker image.."
 DOCKER_IMAGE=demo-app
 CONTAINER_NAME=demo-app-nginx
+sudo docker login --username=username --email=name@company.com 
 sudo docker pull $DOCKER_IMAGE
 sudo docker ps -q --filter ancestor=$DOCKER_IMAGE | xargs sudo docker stop
 sudo docker container run --name $CONTAINER_NAME -p 80:80 -dit $DOCKER_IMAGE
